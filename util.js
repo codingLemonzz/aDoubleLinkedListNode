f59acf2e03145769c74bd46fe8137ddc7ea6b684
zrygoalone <394712702@qq.com>
2018-05-18 17:48:15 +0800

    check number

---
+++ util.js
@@ -0,0 +1,11 @@
+var util = {
+	checkNumber: function(val) {
+		var regPos = /^\d+(\.\d+)?$/; //非负浮点数
+		var regNeg = /^(-(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([0-9]*[1-9][0-9]*)))$/; //负浮点数
+		if(regPos.test(val) || regNeg.test(val)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
